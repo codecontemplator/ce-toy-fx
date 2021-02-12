4711093059fd8049b610b8ddd5e3e289e7117f52
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2021-02-12 20:48:00 +0100

    Some small steps

--- ce-toy-fx.sample.web/client/src/Main.elm
+++ ce-toy-fx.sample.web/client/src/Main.elm
@@ -4,45 +4,67 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Attributes exposing (class, type_)
 import Html.Events exposing (onClick)
-import Bootstrap
 import Html.Keyed as Keyed
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.CDN as CDN
 
 -- https://github.com/evancz/elm-todomvc/blob/master/src/Main.elm
 
 main =
   Browser.sandbox { init = { process = [], nextId = 0 }, update = update, view = view }
 
-type alias AppModel = { process : List MRule, nextId : Int }
+type alias AppModel = { process : List (TreeNode Rule), nextId : Int }
 
-type MRule = MRuleDef { id : Int, name : String, condition : String, projection : String }
-           | MRuleGroup { name : String }
+type TreeNode a = TreeNode { header : String, isExpanded : Bool, id : Int } a
 
-type AppMsg = AddMRuleDef
+type Rule = MRuleDef { name : String, condition : String, projection : String }
+          | MRuleGroup { name : String }
+
+type AppMsg = AddMRuleDef | ExpandRule Int
 
 update : AppMsg -> AppModel -> AppModel
 update msg model =
   case msg of
     AddMRuleDef ->
       let
-        mrule = MRuleDef { id = model.nextId, name = "new rule", condition = "", projection = "" }
+        name = "new rule"
+        mrule = MRuleDef { name = name, condition = "", projection = "" }
+        node = TreeNode { id = model.nextId, header = name, isExpanded = False } mrule
       in
-        { model | nextId = model.nextId + 1, process = model.process ++ [mrule] }
+        { model | nextId = model.nextId + 1, process = model.process ++ [ node ] }
+    ExpandRule id -> 
+      let
+        updateNode (TreeNode n pl) = if id == n.id then TreeNode { n | isExpanded = not n.isExpanded } pl else (TreeNode n pl)
+      in
+        { model | process = List.map updateNode model.process }
 
 view : AppModel -> Html AppMsg
 view model =
-        div []
-            [ Bootstrap.stylesheet
-            , button [ type_ "button", class "btn btn-primary", onClick AddMRuleDef ] [ text "Add Rule" ]
-            , div [] [ viewRuleList model.process ]
-            ]
+        Grid.container []
+          [ CDN.stylesheet
+            , Grid.simpleRow [ Grid.col [] [ button [ type_ "button", class "btn btn-primary", onClick AddMRuleDef ] [ text "Add Rule" ] ] ]
+            , Grid.simpleRow [ Grid.col [] [ viewRuleList model.process ] ]
+          ]
 
-viewRuleList : List MRule -> Html AppMsg
+viewRuleList : List (TreeNode Rule) -> Html AppMsg
 viewRuleList ruleList = 
     let
-        viewKeydRule : MRule -> (String, Html AppMsg)
+        viewKeydRule : (TreeNode Rule) -> (String, Html AppMsg)
         viewKeydRule rule = 
             case rule of
-                MRuleDef rd -> (String.fromInt rd.id, Html.li [ class "list-group-item" ] [text (rd.name)])
+                TreeNode node (MRuleDef rd) -> 
+                  ( String.fromInt node.id
+                  , Html.li [ class "list-group-item" ] 
+                    [ Grid.container [] 
+                        [ Grid.simpleRow 
+                          [ Grid.col [ Col.xsAuto ] [ Html.h4 [] [ text rd.name ] ] 
+                          , Grid.col [ ] [ ]
+                          , Grid.col [ Col.xsAuto ] [ button [ onClick (ExpandRule node.id)] [ text (if node.isExpanded then "Collapse" else "Expand") ] ] 
+                          ]
+                        ]
+                    ]
+                  )
                 _ -> ("", text "")
     in
         Keyed.ul [ class "list-group" ] <|
