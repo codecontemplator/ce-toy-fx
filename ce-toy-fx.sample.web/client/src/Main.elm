4ab64cf93773dd615f1f3bf3e8f2b1311a3c95b9
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2021-02-12 21:57:53 +0100

    Some more progress

--- ce-toy-fx.sample.web/client/src/Main.elm
+++ ce-toy-fx.sample.web/client/src/Main.elm
@@ -15,6 +15,7 @@ import Bootstrap.Form.Checkbox as Checkbox
 import Bootstrap.Form.Radio as Radio
 import Bootstrap.Form.Textarea as Textarea
 import Bootstrap.Form.Fieldset as Fieldset
+import Html.Attributes exposing (hidden)
 
 -- https://github.com/evancz/elm-todomvc/blob/master/src/Main.elm
 
@@ -25,24 +26,30 @@ type alias AppModel = { process : List (TreeNode Rule), nextId : Int }
 
 type TreeNode a = TreeNode { header : String, isExpanded : Bool, id : Int } a
 
-type Rule = MRuleDef { name : String, condition : String, projection : String }
-          | MRuleGroup { name : String }
+type RuleType = Limit | Policy | Group | Vote
+type RuleScope = AllApplicants | AnyApplicant
+type Rule = Rule { type_ : RuleType, name : String, condition : String, projection : String, children : List Rule, scope : RuleScope }
 
-type AppMsg = AddMRuleDef | ExpandRule Int
+type AppMsg = AddRule | ToggleTreeNode Int | UpdateRuleType Int RuleType
 
 update : AppMsg -> AppModel -> AppModel
 update msg model =
   case msg of
-    AddMRuleDef ->
+    AddRule ->
       let
         name = "new rule"
-        mrule = MRuleDef { name = name, condition = "<condition>", projection = "<projection>" }
+        mrule = Rule { type_ = Limit, name = name, condition = "<condition>", projection = "<projection>", children = [], scope = AllApplicants }
         node = TreeNode { id = model.nextId, header = name, isExpanded = False } mrule
       in
         { model | nextId = model.nextId + 1, process = model.process ++ [ node ] }
-    ExpandRule id -> 
+    ToggleTreeNode id -> 
       let
-        updateNode (TreeNode n pl) = if id == n.id then TreeNode { n | isExpanded = not n.isExpanded } pl else (TreeNode n pl)
+        updateNode (TreeNode n pl) = if id == n.id then TreeNode { n | isExpanded = not n.isExpanded } pl else TreeNode n pl
+      in
+        { model | process = List.map updateNode model.process }
+    UpdateRuleType id newType ->
+      let
+        updateNode (TreeNode n (Rule r)) = if id == n.id then TreeNode n (Rule { r | type_ = newType }) else TreeNode n (Rule r)
       in
         { model | process = List.map updateNode model.process }
 
@@ -50,7 +57,7 @@ view : AppModel -> Html AppMsg
 view model =
         Grid.container []
           [ CDN.stylesheet
-            , Grid.simpleRow [ Grid.col [] [ button [ type_ "button", class "btn btn-primary", onClick AddMRuleDef ] [ text "Add Rule" ] ] ]
+            , Grid.simpleRow [ Grid.col [] [ button [ type_ "button", class "btn btn-primary", onClick AddRule ] [ text "Add Rule" ] ] ]
             , Grid.simpleRow [ Grid.col [] [ viewRuleList model.process ] ]
           ]
 
@@ -59,21 +66,28 @@ viewRuleList : List (TreeNode Rule) -> Html AppMsg
 viewRuleList ruleList = 
     let
         viewKeydRule : (TreeNode Rule) -> (String, Html AppMsg)
-        viewKeydRule rule = 
+        viewKeydRule (TreeNode node (Rule rule)) = 
           let
-            viewRuleDetails visible rd =
-              if visible then
+            updateRuleType s = UpdateRuleType node.id (
+                case s of
+                  "Policy" -> Policy
+                  "Limit" -> Limit 
+                  "Group" -> Group
+                  _ -> Vote
+              )
+            viewRuleDetails =
+              if node.isExpanded then
                 [ Form.form [] 
                   [ Form.group []
                       [ Form.label [ Html.Attributes.for "rule-type-selector" ] [ text "Rule type" ]
-                      , Select.select [ Select.id "rule-type-selector" ]
+                      , Select.select [ Select.id "rule-type-selector", Select.onChange updateRuleType]
                           [ Select.item [] [ text "Policy"]
                           , Select.item [] [ text "Limit"]
                           , Select.item [] [ text "Group"]
                           , Select.item [] [ text "Vote"]
                           ]
                       ]
-                  , Form.group []
+                  , Form.group [ Form.attrs [ Html.Attributes.hidden (List.member rule.type_ [ Policy, Limit ] |> not) ] ]
                       [ Form.label [Html.Attributes.for "rule-condition" ] [ text "Condition"]
                       , Input.text [ Input.id "rule-condition" ]
                       , Form.help [] [ text "Example: Vars.Credit < 1000 && Vars.Age >= 20" ]
@@ -87,26 +101,21 @@ viewRuleList ruleList =
                 ]
               else [ ]                
           in
-            case rule of
-                TreeNode node (MRuleDef rd) -> 
-                  ( String.fromInt node.id
-                  , Html.li [ class "list-group-item" ] 
-                    [ Grid.container [] 
-                        (
-                          [ Grid.simpleRow 
-                            [ Grid.col [ Col.xsAuto ] [ button [ class "btn", class "btn-lnk", onClick (ExpandRule node.id) ] [ Html.h4 [] [ text rd.name ] ] ] 
-  --                          , Grid.col [ ] [ ]
-  --                          , Grid.col [ Col.xsAuto ] [ button [ onClick (ExpandRule node.id)] [ text (if node.isExpanded then "Collapse" else "Expand") ] ] 
-                            ]
-                          ] 
-                          ++ 
-                          [ Grid.simpleRow 
-                            [ Grid.col [] (viewRuleDetails node.isExpanded rd) ] 
-                            ]
-                        )
-                    ]
-                  )
-                _ -> ("", text "")
+              ( String.fromInt node.id
+              , Html.li [ class "list-group-item" ] 
+                [ Grid.container [] 
+                    (
+                      [ Grid.simpleRow 
+                        [ Grid.col [ Col.xsAuto ] [ button [ class "btn", class "btn-lnk", onClick (ToggleTreeNode node.id) ] [ Html.h4 [] [ text rule.name ] ] ] 
+--                          , Grid.col [ ] [ ]
+--                          , Grid.col [ Col.xsAuto ] [ button [ onClick (ToggleTreeNode node.id)] [ text (if node.isExpanded then "Collapse" else "Expand") ] ] 
+                        ]
+                      , Grid.simpleRow 
+                        [ Grid.col [] viewRuleDetails ] 
+                        ]
+                    )
+                ]
+              )
     in
         Keyed.ul [ class "list-group" ] <|
                 List.map viewKeydRule ruleList
